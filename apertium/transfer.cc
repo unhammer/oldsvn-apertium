0622de23dded105f3437efd63796c5ce3e5eafcb
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2015-06-17 22:17:52 +0200

    note

--- apertium/transfer.cc
+++ apertium/transfer.cc
@@ -1807,7 +1807,17 @@ Transfer::readToken(FILE *in)
     return input_buffer.next();
   }
 
-  // TODO: there's a lot of assignment and appending going on here, is this a performance issue?
+  // TODO: there's a lot of assignment and appending going on here, is
+  // this a performance issue? idea: alternate way; just keep track of
+  // _last superblank-end_ and _start-of-last-formatblank_. ie input is
+  // [<p>] [{<b>}]^kake…
+  //     i j
+  //
+  // then we want to keep track of the indices i and j above. Then
+  // content[0:i] is super, content[i:j] is free, content[j:] is
+  // format. We just pass the indices to TransferToken's constructor,
+  // and it does substring in the getter (for example, could do it on
+  // init as well but then it has to be stored).
   wstring content, superblank, freeblank, format;
   while(true)
   {
